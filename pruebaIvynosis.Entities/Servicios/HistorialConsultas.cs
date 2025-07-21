e17d81bdb86f78e25395cb6b996e34589d40f367
Jin Shi Ji <JSJI@upv.edu.es>
2025-07-21 16:06:15 +0200

    Establecer los métodos de escritura y consulta del historial

---
+++ pruebaIvynosis.Entities/Servicios/HistorialConsultas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using pruebaIvynosis.Entities.Modelos;
+
+namespace pruebaIvynosis.Entities.Servicios
+{
+    public class HistorialConsultas
+    {
+        private readonly List<Historial> _historial = new();
+
+        public void add (string demandante, string demandado, string resultado)
+        {
+            _historial.Add(new Historial
+            {
+                Demandante = demandante,
+                Demandado = demandado,
+                Resultado = resultado
+            });
+        }
+        public IReadOnlyList<Historial> GetAll ()
+        {
+            return _historial.AsReadOnly(); 
+        }
+    }
+}
