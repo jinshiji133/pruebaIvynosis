4c1307accb130f2910a5a9ce85f5071b1a5357e5
Jin Shi Ji <JSJI@upv.edu.es>
2025-07-21 15:33:15 +0200

    Creación la solución CLI

--- Juicio.CLI/Program.cs
+++ Juicio.CLI/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using pruebaIvynosis.Aplicacion;
+using pruebaIvynosis.Aplicacion.pruebaIvynosis.casosDeUso;
+using pruebaIvynosis.Entities.Servicios;
+
+Console.WriteLine("Introduzca firma de demandante (e.j. KN):");
+var demandante = Console.ReadLine();
+
+Console.WriteLine("Introduzca firma demandado (e.j. NNV):");
+var demandado = Console.ReadLine();
+
+var casoDeUso = new JuicioCasoDeUso(new CalculadoraPuntos());
+var result = casoDeUso.ImprimirGanador(demandante, demandado);
+
+Console.WriteLine($"Result: {result}");
--- Programa.cs
+++ Programa.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿
+using pruebaIvynosis.Aplicacion;
+using pruebaIvynosis.Entities.Servicios;
 
-public class Class1
-{
-	public Class1()
-	{
-	}
-}
+Console.WriteLine("Introduce firmas de demandante (Ej.KN)");
+var demandante = Console.ReadLine();
+
+Console.WriteLine("Introduce firmas de demandado (Ej. NNV):");
+var demandado = Console.ReadLine();
+
+var casoDeUso = new JuicioCasoDeUso(new CalculadoraPuntos());
+var resulttado = casoDeUso.ImprimirGanador(demandante, demandado);
+
+Console.WriteLine($"Result: {resultado}");
