a640acced312479fc8ccf5bc27caead6a8fc8fc6
Jin Shi Ji <JSJI@upv.edu.es>
2025-07-21 15:55:37 +0200

    Borrado de archivos innecesarios

--- Roles.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public enum Roles
-{
-    Rey = 'K',
-    Notario = 'N',
-    Validador = 'V'
-}
--- pruebaIvynosis.Entities/Modelos/Roles.cs
+++ pruebaIvynosis.Entities/Modelos/Roles.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace pruebaIvynosis.Entities.Modelos
 {
+   //Definir todos los roles posibles
     public enum Roles
     {
         Rey = 'K',
